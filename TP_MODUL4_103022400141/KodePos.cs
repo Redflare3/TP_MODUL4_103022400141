490f7efc622d604d337cc9b7f7ef374f07fef012
Solar <redflare31138@gmail.com>
2026-03-15 10:29:03 +0700

    Second Commit

---
+++ TP_MODUL4_103022400141/KodePos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP_MODUL4_103022400141
+{
+    public class KodePos
+    {
+        public enum KodePosType {
+            BATUNUNGGAL,
+            KUJANGSARI,
+            MENGGER,
+            WATES,
+            CIJAURA,
+            JATISARI,
+            MARGASARI,
+            SEKEJATI,
+            KEBONWARU,
+            MALEER
+        }
+        public static int getKodePos(KodePosType type)
+        {
+            int[] kodePosList = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274 };
+            return kodePosList[(int)type];
+        }
+    }
+}
